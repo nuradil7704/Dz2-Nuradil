3f1c63da935fedb39fec610b8e329a0359e8b2dd
nuradil7704 <113079748+nuradil7704@users.noreply.github.com>
2022-11-17 00:36:58 +0600

    Add files via upload

---
+++ DZ2_Nuradil_18-01/my-app/src/pages/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from "./title/Title";
+import Subtitle from "./subtitle/Subtitle";
+
+
+function AboutUs() {
+    return (
+        <>
+           <Title text="Title: hello"/>
+            <Subtitle subtitle="Subtitle: lorem lorem lorem"/>
+        </>
+    );
+}
+
+export default AboutUs;
